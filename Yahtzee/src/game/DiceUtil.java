16e5476b1ba0918cbed03c2556ad46a32346d32b
Christopher Cushman <chriscush765@gmail.com>
2015-06-02 13:19:30 -0400

    Added dice random stuff lol

--- Yahtzee/src/game/DiceUtil.java
+++ Yahtzee/src/game/DiceUtil.java
@@ -3,37 +3,49 @@
  */
 package game;
 
+import java.util.List;
+
 /**
  * @author 310843
  *
  */
 public class DiceUtil {
-	
-	private static int[] dice = new int[5];
-	
+
 	/**
-	 * Returns a new roll of the dice except those that are kept
-	 * @param indeciesNotRolled - the indexes of the dice that are "kept" (Not Re-rolled) 
-	 * @return
-	 * Pre-Condition: integers in indeciesNotRolled < 6;
+	 * Populates an array with a random value from 1 - 6 ONLY if the current value is 0
+	 * Leaves nonzero values alone
+	 * @param dice The array to populate / repopulate
+	 * @return a populated array
 	 */
-	public static int[] getDiceArray(int[] indeciesNotRolled) {
-		
-		return null;
-		
+	public static int[] populateDiceArray(int[] dice) {
+		for (int i = 0; i < dice.length; i++) {
+			if (dice[i] == 0) {
+				dice[i] = 1 + (int) (Math.random() * 6);
+			}
+		}
+		return dice;
 	}
 	
 	/**
+	 * Returns a random array of ints from 1 to 6, with 6 ints in total
+	 * @return
+	 */
+	public static int[] getRandomDice(){
+		return populateDiceArray(new int[6]);
+	}
+
+	/**
 	 * Calculates All possible scores based on the current dice state
+	 * 
 	 * @return
 	 */
 	public static int[] calculateScores() {
-		
+
 		/*
 		 * GO AWAY JUSTIN IS DOING THIS LOL
 		 */
 		return null;
-		
+
 	}
 
 }
