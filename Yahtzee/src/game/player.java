7d9144a1ad6fbc0b16f41e3b3295162fa099096c
155053 <155053@HSS-101-01.westwindsor.k12.nj.us>
2015-06-02 09:36:08 -0400

    WEE TABLE

--- Yahtzee/src/game/player.java
+++ Yahtzee/src/game/player.java
@@ -1,6 +1,6 @@
 package game;
 
-import java.util.ArrayList;
+
 
 public class player {
 	public void roll(){
@@ -9,5 +9,22 @@ public class player {
 	public void reRoll(){
 		
 	}
+	/*
+	 * 
+	 *Aces				Any combination										The sum of dice with the number 1	
+	 *Twos				Any combination										The sum of dice with the number 2	
+	 *Threes			Any combination										The sum of dice with the number 3	
+	 *Fours				Any combination										The sum of dice with the number 4	
+	 *Fives				Any combination										The sum of dice with the number 5	
+	 *Sixes				Any combination										The sum of dice with the number 6
+	 *Three-Of-A-Kind	At least three dice the same						Sum of all dice
+	 *Four-Of-A-Kind	At least four dice the same							Sum of all dice	
+	 *Full House		Three of one number and two of another				25	
+	 *Small Straight	Four sequential dice (1-2-3-4, 2-3-4-5, or 3-4-5-6)	30	
+	 *Large Straight	Five sequential dice (1-2-3-4-5 or 2-3-4-5-6)		40	
+	 *Yahtzee			All five dice the same								50	
+	 *Chance			Any combination										Sum of all dice
+	 * 
+	 */
 	
 }
