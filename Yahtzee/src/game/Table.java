7adf82accdc33cfd4e0ae5eac6ec380ee2d9d331
Christopher Cushman <chriscush765@gmail.com>
2015-06-02 09:26:48 -0400

    Updated table class with method headers

--- Yahtzee/src/game/Table.java
+++ Yahtzee/src/game/Table.java
@@ -1,6 +1,31 @@
 package game;
 
-public class Table 
-{
+public class Table {
+
+	public Table(){
+		
+	}
+	
+	/**
+	 * Prepares the game for play
+	 */
+	public void setupGame(){
+		
+	}
+	
+	/**
+	 * Main loop of the game
+	 */
+	public void runGame(){
+		
+	}
+	
+	/**
+	 * Runs when game is over
+	 * Displays scores and such
+	 */
+	public void endGame(){
+		
+	}
 	
 }
