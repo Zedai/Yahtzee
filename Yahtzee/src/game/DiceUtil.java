49cf215e7a26102ae77391edd73be9eb3a7c2c3f
310843 <310843@HSS-101-07.westwindsor.k12.nj.us>
2015-06-08 08:49:49 -0400

    some idiot made yahtzee always return 0

--- Yahtzee/src/game/DiceUtil.java
+++ Yahtzee/src/game/DiceUtil.java
@@ -11,43 +11,7 @@ import java.util.List;
  *
  */
 public class DiceUtil {
-	/*	int sumDice = 0;							
-	for(int x = 0; x < dice.length; x++)
-		sumDice += dice[x];
 	
-	int duplicates = 0;
-	for(int j = 0; j < dice.length; j++)
-		for(int k = 0; k < dice.length; k++)
-			if(j!=k && dice[j] == dice[k])
-				duplicates++;
-	
-	possibleScoreSet[6] = sumDice;
-	if(duplicates == 3)
-		possibleScoreSet[0] = sumDice;
-	if(duplicates == 4)
-	{
-		possibleScoreSet[0] = sumDice;
-		possibleScoreSet[1] = sumDice;
-	}
-	if(duplicates == 3 && true)
-		
-		
-		
-		
-		
-	for(int i=0;i<dice.length;i++)	{
-		
-		if(dice[i] == dice[i-1])
-			possibleScoreSet[5] = 50;
-		else 
-			possibleScoreSet[5] = 0;
-		
-			
-		
-		
-	}
-		
-		*/
 	/**
 	 * Populates an array with a random value from 1 - 6 ONLY if the current value is 0
 	 * Leaves nonzero values alone
@@ -79,28 +43,6 @@ public class DiceUtil {
 	//hypothetical dice because I don't see how scores will be calculated if nothing is passed in
 	public static int[] calculateScores(int[] dice) 
 	{
-		/*
-		 * GO AWAY JUSTIN & ARYAK ARE DOING THIS LOL tooo laaaate
-		 * 
-		 */
-		
-		//7 types of scoring possible
-		/*Order goes according to indexes:
-		 *Ones
-		 *Twos
-		 *Threes
-		 *Fours
-		 *Fives
-		 *Sixes
-		 *3 of a kind
-		 *4 of a kind
-		 *full house
-		 *small Straight
-		 *Large Straight
-		 *Yahtzee
-		 *Chance
-		 */
-	
 		
 		return new int[] {				
 				oneToSix(1,dice),
@@ -173,7 +115,7 @@ public class DiceUtil {
 			if (dice[i] != key)
 				return 0;
 		}
-		return 0;
+		return 50;
 	}
 	
 	private static int oneToSix(int number, int[] dice)
