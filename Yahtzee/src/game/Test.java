77ad8aafba5403fb340ec1104fe6b84ce74e1a53
310843 <310843@HSS-101-07.westwindsor.k12.nj.us>
2015-06-08 09:01:04 -0400

    dfbdbrtdrbra

--- Yahtzee/src/game/Test.java
+++ Yahtzee/src/game/Test.java
@@ -1,15 +1,16 @@
 package game;
 
+import java.util.Arrays;
+
 public class Test {
-
+	
 	public static void main(String[] args) {
-		int[] dice = {5,4,6,2,3,1};
-		int[] newdice = UI.promptForReRoll(dice, 0);
 		
-		for (int i : newdice) {
-			System.out.print(i + ", ");
-		}
-
+		int[] dice = {1,2,3,4,5,6};
+		int [] newshet = DiceUtil.calculateScores(dice);
+		Arrays.toString(newshet);
+		
+		
 	}
 
 }
